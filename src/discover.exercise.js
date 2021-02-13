1623f71c1f55f14337afddb305bb2973940c8d99
Davy Hausser <davy.hausser@gmail.com>
2021-02-13 18:16:50 +0100

    2. 💯 use the useAsync hook

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -6,36 +6,22 @@ import Tooltip from '@reach/tooltip'
 import {FaSearch, FaTimes} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
+import {useAsync} from 'utils/hooks'
 import {client} from './utils/api-client'
 import * as colors from 'styles/colors'
 
 function DiscoverBooksScreen() {
-  const [status, setStatus] = React.useState('idle')
-  const [data, setData] = React.useState()
-  const [error, setError] = React.useState()
   const [query, setQuery] = React.useState()
   const [queried, setQueried] = React.useState(false)
 
-  const isLoading = status === 'loading'
-  const isSuccess = status === 'success'
-  const isError = status === 'error'
+  const {data, error, run, isLoading, isError, isSuccess} = useAsync()
 
   React.useEffect(() => {
     if (!queried) {
       return
     }
-    setStatus('loading')
-    client(`books?query=${encodeURIComponent(query)}`).then(
-      responseData => {
-        setData(responseData)
-        setStatus('success')
-      },
-      errorData => {
-        setError(errorData)
-        setStatus('error')
-      },
-    )
-  }, [query, queried])
+    run(client(`books?query=${encodeURIComponent(query)}`))
+  }, [query, queried, run])
 
   function handleSearchSubmit(event) {
     event.preventDefault()
