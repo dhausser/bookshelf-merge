22975e0545756784d5f1d63bbbea14733a881a26
Davy Hausser <davy.hausser@gmail.com>
2021-02-23 23:24:11 +0100

    7. 💯 Add optimistic updates and recovery

--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -23,7 +23,13 @@ function useListItem(user, bookId) {
   return listItems.find(li => li.bookId === bookId) ?? null
 }
 
+// onMutate(variables) {},
+// onError(err, variables, onMutateValue) {},
+// onSettled(data, error, variables, onMutateValue) {},
+
 const defaultMutationOptions = {
+  onError: (err, variables, recover) =>
+    typeof recover === 'function' ? recover() : null,
   onSettled: () => queryCache.invalidateQueries('list-items'),
 }
 
@@ -35,14 +41,40 @@ function useUpdateListItem(user, options) {
         data: updates,
         token: user.token,
       }),
-    {...defaultMutationOptions, ...options},
+    {
+      onMutate(newItem) {
+        const previousItems = queryCache.getQueryData('list-items')
+
+        queryCache.setQueryData('list-items', old => {
+          return old.map(item => {
+            return item.id === newItem.id ? {...item, ...newItem} : item
+          })
+        })
+
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...defaultMutationOptions,
+      ...options,
+    },
   )
 }
 
 function useRemoveListItem(user, options) {
   return useMutation(
     ({id}) => client(`list-items/${id}`, {method: 'DELETE', token: user.token}),
-    {...defaultMutationOptions, ...options},
+    {
+      onMutate(removedItem) {
+        const previousItems = queryCache.getQueryData('list-items')
+
+        queryCache.setQueryData('list-items', old => {
+          return old.filter(item => item.id !== removedItem.id)
+        })
+
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...defaultMutationOptions,
+      ...options,
+    },
   )
 }
 
