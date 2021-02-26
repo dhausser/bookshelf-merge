9164a3e1fbefac519d5093461cec81d8e9b3e3d5
Davy Hausser <davy.hausser@gmail.com>
2021-02-26 15:31:12 +0100

    3. 💯 colocate global providers

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,29 +2,14 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {ReactQueryConfigProvider} from 'react-query'
-import {AuthProvider} from 'context/auth-context'
+import {AppProviders} from 'context'
 import {App} from './app'
-
-const queryConfig = {
-  queries: {
-    useErrorBoundary: true,
-    refetchAllOnWindowFocus: false,
-    retry(failureCount, error) {
-      if (error.status === 404) return false
-      else if (failureCount < 2) return true
-      else return false
-    },
-  },
-}
 
 loadDevTools(() => {
   ReactDOM.render(
-    <ReactQueryConfigProvider config={queryConfig}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ReactQueryConfigProvider>,
+    <AppProviders>
+      <App />
+    </AppProviders>,
     document.getElementById('root'),
   )
 })
