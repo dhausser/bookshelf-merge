c663f4580f47d92678bb290da5e37c5a577cb118
Davy Hausser <davy.hausser@gmail.com>
2021-02-28 11:05:11 +0100

    Test formatDate

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,6 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  const date = new Date()
+  expect(formatDate(date)).toBe('Feb 21')
+})
