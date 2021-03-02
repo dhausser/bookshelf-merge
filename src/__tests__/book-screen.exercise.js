0f75942c412b09080cd5437b9a54290d39d62cc3
Davy Hausser <davy.hausser@gmail.com>
2021-03-02 18:39:33 +0100

    Integration testing exercise complete

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {render, screen, waitFor} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import {queryCache} from 'react-query'
 import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
@@ -11,37 +11,56 @@ afterEach(() => {
   auth.logout()
 })
 
-test('renders all the book information', () => {
-  localStorage.setItem('localStorageKey', auth.localStorageKey)
-
+test('renders all the book information', async () => {
   const user = buildUser()
-  const book = buildBook()
-  window.history.pushState({}, 'book', `/book/${book.id}`)
+  window.localStorage.setItem(auth.localStorageKey, 'SOME_FAKE_TOKEN')
 
+  const book = buildBook()
+  const route = `/book/${book.id}`
+  window.history.pushState({}, 'book', route)
+
+  const originalFetch = window.fetch
   window.fetch = async (url, config) => {
-    switch (url) {
-      case `/me`: {
-        return Promise.resolve({ok: true, json: async () => ({user})})
+    if (url.endsWith('/bootstrap')) {
+      return {
+        ok: true,
+        json: async () => ({
+          user: {...user, token: 'SOME_FAKE_TOKEN'},
+          listItems: [],
+        }),
       }
-      case `/list-items`: {
-        return Promise.resolve({ok: true, json: async () => ({listItems: []})})
-      }
-      case `/book/${book.id}`: {
-        return Promise.resolve({ok: true, json: async () => ({book})})
-      }
-      default: {
-        break
-      }
+    } else if (url.endsWith(`/books/${book.id}`)) {
+      return {ok: true, json: async () => ({book})}
     }
+    return originalFetch(url, config)
   }
 
-  const {container} = render(<App />, {wrapper: <AppProviders />})
+  render(<App />, {wrapper: AppProviders})
 
-  waitFor(() => {
-    if (queryCache.isFetching) {
-      throw new Error('Still fetching')
-    }
-  })
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-  screen.debug()
+  expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  expect(screen.getByText(book.author)).toBeInTheDocument()
+  expect(screen.getByText(book.publisher)).toBeInTheDocument()
+  expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+  expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+    'src',
+    book.coverImageUrl,
+  )
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('button', {name: /mark as unread/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByRole('textbox', {name: /notes/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+  expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
 })
