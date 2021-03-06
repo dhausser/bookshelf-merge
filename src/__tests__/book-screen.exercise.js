362c204e9deb068234074e4392df4c989a1dd0a3
Davy Hausser <davy.hausser@gmail.com>
2021-03-06 15:19:48 +0100

    Show Error when Load Fails

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -160,3 +160,36 @@ test('can edit a note', async () => {
     notes: newNotes,
   })
 })
+
+// describe('console errors', () => {
+//   beforeAll(() => {
+//     jest.spyOn(console, 'error').mockImplementation(() => {})
+//   })
+
+//   afterAll(() => {
+//     console.error.mockRestore()
+//   })
+
+test('shows an error message when the book fails to load', async () => {
+  const book = {id: 'BAD_ID'}
+  await renderBookScreen({book, listItem: null})
+
+  expect((await screen.findByRole('alert')).textContent).toMatchInlineSnapshot(
+    `"There was an error: Book not found"`,
+  )
+  // expect(console.error).toHaveBeenCalled()
+})
+
+test('note update failures are displayed', async () => {
+  // const apiURL = process.env.REACT_APP_API_URL
+  // const testErrorMessage = '__test_error_message__'
+  // server.use(
+  //   rest.put(`${apiURL}/list-items/:listItemId`, async (req, res, ctx) => {
+  //     return res(
+  //       ctx.status(400),
+  //       ctx.json({status: 400, message: testErrorMessage}),
+  //     )
+  //   }),
+  // )
+})
+// })
