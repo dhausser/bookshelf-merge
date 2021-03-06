e8e020e233e1b4ae93d10d439caf5c7662cc49d8
Davy Hausser <davy.hausser@gmail.com>
2021-03-06 12:53:34 +0100

    Global Utils

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,62 +1,14 @@
 import * as React from 'react'
 import {
-  render as rtlRender,
+  render,
   screen,
-  waitForElementToBeRemoved,
-} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {queryCache} from 'react-query'
-import * as auth from 'auth-provider'
-import {buildUser, buildBook} from 'test/generate'
+  waitForLoadingToFinish,
+  userEvent,
+} from 'test/app-test-utils'
+import {buildBook} from 'test/generate'
 import * as booksDB from 'test/data/books'
-import * as usersDB from 'test/data/users'
-import * as listItemsDB from 'test/data/list-items'
 import {formatDate} from 'utils/misc'
-import {AppProviders} from 'context'
 import {App} from 'app'
-
-// general cleanup
-afterEach(async () => {
-  queryCache.clear()
-  await Promise.all([
-    auth.logout(),
-    usersDB.reset(),
-    booksDB.reset(),
-    listItemsDB.reset(),
-  ])
-})
-
-async function render(ui, {route = '/list', user, ...renderOptions} = {}) {
-  user = typeof user === 'undefined' ? await loginAsUser() : user
-
-  window.history.pushState({}, 'Test page', route)
-
-  const returnValue = {
-    ...rtlRender(ui, {wrapper: AppProviders, ...renderOptions}),
-    user,
-  }
-
-  await waitForLoadingToFinish()
-
-  return returnValue
-}
-
-async function loginAsUser() {
-  const user = buildUser()
-  await usersDB.create(user)
-  const authUser = await usersDB.authenticate(user)
-  // this is what our auth provider does to persist the user's
-  // logged in state so it can give us a token without making a request
-  // every provider will be different and you'll need to adjust this
-  // to whatever they do (you may even have to mock more of their functions).
-  window.localStorage.setItem(auth.localStorageKey, authUser.token)
-}
-
-const waitForLoadingToFinish = () =>
-  waitForElementToBeRemoved(() => [
-    ...screen.queryAllByLabelText(/loading/i),
-    ...screen.queryAllByText(/loading/i),
-  ])
 
 test('renders all the book information', async () => {
   const book = await booksDB.create(buildBook())
