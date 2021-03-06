636d806fffdcd0966cd23dd5ae218848aa37a3a9
Davy Hausser <davy.hausser@gmail.com>
2021-03-06 16:33:09 +0100

    Mark Book as Read and Rate

--- cypress/e2e/smoke.exercise.js
+++ cypress/e2e/smoke.exercise.js
@@ -30,28 +30,28 @@ describe('smoke', () => {
 
     cy.findByRole('main').within(() => {
       cy.findAllByRole('listitem').should('have.length', 1)
-      // cy.findByRole('link', {name: /voice of war/i}).click()
+      cy.findByRole('link', {name: /voice of war/i}).click()
     })
 
-    // cy.findByRole('textbox', {name: /notes/i}).type('This is an awesome book')
-    // cy.findByLabelText(/loading/i).should('exist')
-    // cy.findByLabelText(/loading/i).should('not.exist')
+    cy.findByRole('textbox', {name: /notes/i}).type('This is an awesome book')
+    cy.findByLabelText(/loading/i).should('exist')
+    cy.findByLabelText(/loading/i).should('not.exist')
 
-    // cy.findByRole('button', {name: /mark as read/i}).click()
+    cy.findByRole('button', {name: /mark as read/i}).click()
 
-    // // the radio buttons are fancy and the inputs themselves are visually hidden
-    // // in favor of nice looking stars, so we have to force the click.
-    // cy.findByRole('radio', {name: /5 stars/i}).click({force: true})
+    // the radio buttons are fancy and the inputs themselves are visually hidden
+    // in favor of nice looking stars, so we have to force the click.
+    cy.findByRole('radio', {name: /5 stars/i}).click({force: true})
 
-    // cy.findByRole('navigation').within(() => {
-    //   cy.findByRole('link', {name: /finished books/i}).click()
-    // })
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
 
-    // cy.findByRole('main').within(() => {
-    //   cy.findAllByRole('listitem').should('have.length', 1)
-    //   cy.findByRole('radio', {name: /5 stars/i}).should('be.checked')
-    //   cy.findByRole('link', {name: /voice of war/i}).click()
-    // })
+    cy.findByRole('main').within(() => {
+      cy.findAllByRole('listitem').should('have.length', 1)
+      cy.findByRole('radio', {name: /5 stars/i}).should('be.checked')
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
 
     // cy.findByRole('button', {name: /remove from list/i}).click()
     // cy.findByRole('textbox', {name: /notes/i}).should('not.exist')
